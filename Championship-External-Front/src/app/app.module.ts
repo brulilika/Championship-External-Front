90f464c99de345b8ea5523985e64c8bf1960c9f5
Bruna Lika <brunalika@msn.com>
2023-02-22 19:43:58 -0300

    Syncfusion credentials

--- Championship-External-Front/src/app/app.module.ts
+++ Championship-External-Front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GridModule, PagerModule } from '@syncfusion/ej2-angular-grids';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GridModule, PagerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
