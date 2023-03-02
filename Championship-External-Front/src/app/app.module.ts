c4161203bafc3cb9e6a37524db827ce5e2b3a076
Bruna Lika <brunalika@msn.com>
2023-03-01 22:37:18 -0300

    dash - carousel and dash cards

--- Championship-External-Front/src/app/app.module.ts
+++ Championship-External-Front/src/app/app.module.ts
@@ -9,18 +9,22 @@ import { MainPage } from './pages/main/main.component';
 import { TicketPage } from './pages/ticket/ticket.component';
 import { AppRoutingModule } from './utils/app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DashCarousel } from "./components/carosel/carosel.component";
+import { MainCard } from "./components/maincard/maincard.component";
 @NgModule({
-  declarations: [
-    AppComponent, Navbar,
-    TicketPage, ChampionshipPage,MainPage
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent, Navbar,
+        TicketPage, ChampionshipPage, MainPage
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        NgbModule,
+        DashCarousel,
+        MainCard
+    ]
 })
 export class AppModule { }
---
+++ Championship-External-Front/src/app/components/maincard/maincard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+	selector: 'main-card',
+	standalone: true,
+	imports: [],
+	templateUrl: './maincard.component.html',
+})
+export class MainCard {
+    
+}
