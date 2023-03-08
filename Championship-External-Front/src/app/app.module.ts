726b6c1e4a38f4c874073915f47b095c3aef5c39
Bruna Lika <brunalika@msn.com>
2023-03-07 22:41:21 -0300

    champ-detail - inital template

--- Championship-External-Front/src/app/app.module.ts
+++ Championship-External-Front/src/app/app.module.ts
@@ -8,24 +8,30 @@ import { ChampionshipPage } from './pages/championship/championship.component';
 import { MainPage } from './pages/main/main.component';
 import { TicketPage } from './pages/ticket/ticket.component';
 import { AppRoutingModule } from './utils/app-routing.module';
-import { NgbAlertModule, NgbProgressbar } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionModule, NgbAlertModule, NgbProgressbar } from '@ng-bootstrap/ng-bootstrap';
 import { DashCarousel } from "./components/carosel/carosel.component";
 import { MainCard } from "./components/maincard/maincard.component";
 import { TicketCard } from './components/ticketcard/ticket.component';
 import { NgIconsModule } from '@ng-icons/core';
 import { featherAirplay } from '@ng-icons/feather-icons';
-import { heroTicket, heroUsers } from '@ng-icons/heroicons/outline';
+import { heroTicket } from '@ng-icons/heroicons/outline';
 import { ChampcardComponent } from './components/champcard/champcard.component';
 import { ChampionshipDetailPage } from './pages/championship/detail/championship-detail.component';
+
+import { TableMatch } from './components/table/matchtable.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialExampleModule } from 'material.module';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
     declarations: [
         AppComponent,
         Navbar,
         TicketPage, 
         ChampionshipPage,
-         MainPage, 
-         ChampcardComponent,
-         ChampionshipDetailPage
+        MainPage, 
+        ChampcardComponent,
+        ChampionshipDetailPage,
+        TableMatch
     ],
     providers: [],
     bootstrap: [AppComponent],
@@ -36,10 +42,13 @@ import { ChampionshipDetailPage } from './pages/championship/detail/championship
         HttpClientModule,
         NgbProgressbar,
         NgbAlertModule,
+        NgbAccordionModule,
         DashCarousel,
         MainCard,
-        TicketCard
-        
+        TicketCard,
+        BrowserAnimationsModule,
+        MaterialExampleModule,
+        MatNativeDateModule
     ]
 })
 export class AppModule { }
